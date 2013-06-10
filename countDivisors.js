18d21db09c27f4b9365fe339eb38c1b76b234a02
Arvid Häggqvist <arvid.haggqvist@gmail.com>
2013-06-10 17:55:56 +0200

    Initial commit

---
+++ countDivisors.js
@@ -0,0 +1,31 @@
+/* Based on the sieve of Eratosthenes, this program calculates how many available divisors a given number n has */
+
+function countDivisors(n) {
+    var LIMIT = n;
+    var numbers = [];
+    
+    for(i=0;i<LIMIT;i++) {
+        numbers[i] = i+2;
+    }
+    
+    for (i=0;i<LIMIT;i++){
+        if(LIMIT % numbers[i] !== 0) {
+            numbers[i]=-1;
+        }
+        if (numbers[i]!== -1){
+            if(LIMIT % numbers[i] !== 0) {
+                for (j=2*numbers[i]-2;j<LIMIT;j+=numbers[i]) {
+                    numbers[j]=-1;
+                }
+            }
+        }
+    }
+    
+    var sum = 0, divisibles = [];
+    for (i=0;i<LIMIT;i++) {
+        if (numbers[i]!== -1) {
+            divisibles.push(numbers[i]);
+        }
+    }
+    return divisibles.length + 1; // Plus one because count one aswell
+}
